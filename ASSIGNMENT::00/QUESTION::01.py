854433c1e95735878d327be07422b664470ac1f8
MukheshTeja <mukhesh2006teja@gmail.com>
2025-01-23 15:34:46 +0530

    Create QUESTION::01.py

---
+++ ASSIGNMENT::00/QUESTION::01.py
@@ -0,0 +1,5 @@
+printf("Addtion of two numbers")
+number_1=int(input("enter the first number::"))
+number_2=int(input("enter the second number::"))
+sum=number_1+number_2
+print("the sum of the two numbers is",sum)
