134e1483ec601edcd2ce33a731a9dcee95750bf5
MukheshTeja <mukhesh2006teja@gmail.com>
2025-01-23 15:36:48 +0530

    Create QUESTION::04.py

---
+++ ASSIGNMENT::00/QUESTION::04.py
@@ -0,0 +1,8 @@
+print("swaping two numbers without the thrid variable")
+number_1=int(input("enter the first number::"))
+number_2=int(input("enter the second number::"))
+print("the first number before swap is",number_1)
+print("the second number before swap is",number_2)
+number_1,number_2=number_2,number_1
+print("the first number now is",number_1)
+print("the second number now is",number_2)
