5c1e6b28c23cb813c3ad63224fa6b0f5bad415b3
MukheshTeja <mukhesh2006teja@gmail.com>
2025-01-23 15:37:19 +0530

    Create QUESTION::05.py

---
+++ ASSIGNMENT::00/QUESTION::05.py
@@ -0,0 +1,7 @@
+print("the table of any number")
+table_number=int(input("enter the number::"))
+limit=int(input("enter until which number of multiples are needed::"))
+for i in range (limit+1):
+    print(table_number * i)
+
+    
