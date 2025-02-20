590068b4165e6fe68d64707a4faf784442e59e09
MukheshTeja <mukhesh2006teja@gmail.com>
2025-02-20 14:11:15 +0530

    Update QUESTION::01.py

--- ASSIGNMENT::05/QUESTION::01.py
+++ ASSIGNMENT::05/QUESTION::01.py
@@ -1,1 +1,8 @@
 
+def maxxor(num, check):
+    for i in range(num, check + 1):
+        for j in range(i, check + 1): 
+            element = i ^ j
+            print(f"{i} xor {j} =", element)
+
+maxxor(10, 15)
