75d7583192bbe96aacd54b2073f9cc7985fa6925
MukheshTeja <mukhesh2006teja@gmail.com>
2025-02-20 14:19:03 +0530

    Update QUESTION::01.py

--- ASSIGNMENT::05/QUESTION::01.py
+++ ASSIGNMENT::05/QUESTION::01.py
@@ -1,8 +1,11 @@
-
 def maxxor(num, check):
-    for i in range(num, check + 1):
-        for j in range(i, check + 1): 
-            element = i ^ j
-            print(f"{i} xor {j} =", element)
+    max_xor = 0 
+    for i in range(num, check + 1): 
+        for j in range(i, check + 1):
+            xor_value = i ^ j
+            if xor_value > max_xor: 
+                max_xor = xor_value 
+            print(i, "xor", j, "=", xor_value) 
+    print("Max XOR value:", max_xor)
 
 maxxor(10, 15)
