0047c9ec4c71f4c547f2e5d61d45374e784a2ba8
MukheshTeja <mukhesh2006teja@gmail.com>
2025-04-23 23:34:30 +0530

    Create Question_8.py

---
+++ ASSIGNMENT::07/Question_8.py
@@ -0,0 +1,14 @@
+def decode(s, indx=0, path='', res=None):
+    if res is None: res = []
+    if indx == len(s): res.append(path); return
+    num1 = int(s[indx])
+    if 1 <= num1 <= 9: decode(s, indx + 1, path + chr(64 + num1), res)
+    if indx + 1 < len(s):
+        num2 = int(s[indx:indx + 2])
+        if 10 <= num2 <= 26: decode(s, indx + 2, path + chr(64 + num2), res)
+    return res
+s = "11106"
+result = decode(s)
+for r in result: 
+    print(r)
+                        
